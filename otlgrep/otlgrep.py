eeb2cb3f841cf0bf08ca333ba4f54daafe1e6197
Noel Henson <noel@noels-lab.com>
2006-02-19 18:10:46 +0000

    Added licence information.

--- otlgrep/otlgrep.py
+++ otlgrep/otlgrep.py
@@ -4,8 +4,8 @@
 #
 # Copyright 2005 Noel Henson All rights reserved
 #
-# $Revision: 1.2 $
-# $Date: 2005/11/02 23:37:37 $
+# $Revision: 1.3 $
+# $Date: 2005/11/03 13:48:10 $
 # $Author: noel $
 # $Source: /home/noel/active/otlgrep/RCS/otlgrep.py,v $
 # $Locker: noel $
@@ -56,6 +56,19 @@
 #		Dogs
 #			Kirby
 #			Hoover
+#
+#   This program is free software; you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation; either version 2 of the License.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with this program; if not, write to the Free Software
+#   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
 
 ###########################################################################
 # include whatever mdules we need
@@ -109,8 +122,8 @@ def showUsage():
 def showVersion():
    print
    print "RCS"
-   print " $Revision: 1.2 $"
-   print " $Date: 2005/11/02 23:37:37 $"
+   print " $Revision: 1.3 $"
+   print " $Date: 2005/11/03 13:48:10 $"
    print " $Author: noel $"
    print
 
